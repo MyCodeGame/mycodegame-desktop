79f2dc8ba494094e3773a56a5ef78e9712d68342
pxl <1278518555@qq.com>
2020-03-03 22:43:42 +0800

    react

---
+++ precache-manifest.649271c47f8eb6a21f733309e6152236.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a4aaf13802044a084fb93c600148c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c73b3627473da0bdd89",
+    "url": "/static/css/2.692958a7.chunk.css"
+  },
+  {
+    "revision": "efecad4ac5a4dec5bbe8",
+    "url": "/static/css/main.22d39155.chunk.css"
+  },
+  {
+    "revision": "8c73b3627473da0bdd89",
+    "url": "/static/js/2.2c0ce22d.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.2c0ce22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efecad4ac5a4dec5bbe8",
+    "url": "/static/js/main.ceaab0cb.chunk.js"
+  },
+  {
+    "revision": "e295a9b2bfb566286667",
+    "url": "/static/js/runtime-main.9ab6c989.js"
+  }
+]);
